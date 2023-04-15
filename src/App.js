d486ed50b8d5c50852d75d70199d91fec993c56c
Van Khanh <223344nguyen@gmail.com>
2023-04-15 19:16:33 +0700

    Add DB

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const server = http.createServer((req, res) => {
 });
 
 // Chạy server tại cổng 3000
-server.listen(3000, () => {
+server.listen(3001, () => {
   console.log('Server is running at port 3000');
 });
 // Đọc nội dung file html
@@ -24,13 +24,13 @@ const homePage2 = fs.readFileSync('src/Admin/index.html');
 const server2 = http.createServer((req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(homePage);
+    res.write(homePage2);
     res.end();
   }
 });
 
 // Chạy server tại cổng 3000
-server2.listen(8000, () => {
+server2.listen(8001, () => {
   console.log('Admin client is running at port 8000');
 });
   
