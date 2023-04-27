e5f014958bbac1382924ec8a11964d1fed0850bd
Van Khanh <223344nguyen@gmail.com>
2023-04-27 14:34:32 +0700

    sửa tất cả file lại theo yêu cầu
    muốn viết hay thêm gì thì tạo thêm nhánh từ nhánh này

---
+++ db/mongodb/database.js
@@ -0,0 +1,20 @@
+
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+//đường dẫn mongoDb
+const mongoString = 'mongodb://localhost:27017/Final_Project';
+
+mongoose.connect(mongoString, { useNewUrlParser: true });
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log(`Database Connected ${new Date().toLocaleTimeString()}`);
+})
+const app = express();
+app.use(express.json());
+
