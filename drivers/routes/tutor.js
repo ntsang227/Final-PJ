cadb8e764d670fee009302dcf269a22f33b4f4cd
Van Khanh <223344nguyen@gmail.com>
2023-07-03 21:46:42 +0700

    update

--- drivers/routes/tutor.js
+++ drivers/routes/tutor.js
@@ -11,13 +11,13 @@ const router = express.Router();
 //Yêu cầu chuyển hướng
 router.get('/', checkMember, function (req, res) {
   req.session.loggedin_tutor = false;
-  res.render('User/main/index.ejs', { email: req.session.email });
+  res.render('User/main/index.ejs', { email : req.session.email });
 });
 // Chuyển hướng đến trang home tutor 
 
 router.get('/home', checkMember, async (req, res) => {
   try {
-    const courses = await Course.find();
+    const courses = await Course.find({status: 'active'});
     res.render('User/main/index.ejs',
       {
         courses,
@@ -26,30 +26,26 @@ router.get('/home', checkMember, async (req, res) => {
   catch (error) {
     res.status(500).json({ message: error.message })
   }
-});
+})
 router.get('/courses', checkMember, function (req, res) {
   res.render('User/main/course.ejs', { email: req.session.email });
-});
-router.get('/calendar', checkMember, function (req, res) {
-  res.render('User/main/calendar.ejs', { email: req.session.email });
 });
 router.get('/detail-course', checkMember, function (req, res) {
   res.render('User/main/detail-courses.ejs', { email: req.session.email });
 });
-router.get('/detail-courses/:id', checkMember, async function (req, res) {
+router.get('/detail-courses/:id',checkMember, async function(req, res) {
   try {
-    const id = req.params.id;
-    const courses = await Course.findById(id);
-    res.render('User/main/detail-courses',
-      {
-        courses,
-      });
+      const id = req.params.id;
+      const courses = await Course.findById(id);
+          res.render('User/main/detail-courses', 
+          {
+              courses ,
+              });
   }
   catch (error) {
-    res.redirect('/courses/');
+      res.redirect('/courses/');
   }
-});
-
+}); 
 //Chuyển hướng đến đăng kí thành viên
 router.get('/register', function (req, res) {
   res.render('User/signup/index.ejs', { message: '' });
@@ -59,11 +55,9 @@ router.get('/register', function (req, res) {
 router.get('/login', function (req, res) {
   res.render('User/login/index.ejs', { message: '' });
 });
-
 //Post đăng kí tài khoản
 router.post('/register', async (req, res) => { //NOSONAR
   const tutor = new Tutor({
-    name: req.body.name,
     username: req.body.username,
     email: req.body.email,
     password: req.body.password
@@ -75,13 +69,13 @@ router.post('/register', async (req, res) => { //NOSONAR
   });
   const email = req.body.email;
   const username = req.body.username;
-  const existingUsername = await Tutor.findOne({ username });
+  const existingUsername = await Tutor.findOne({ username});
   const existingEmail = await Tutor.findOne({ email });
   if (existingEmail) {
-    return res.render('User/signup', { message: 'Email đã được đăng ký.' });
+    return res.render('User/signup',{ message: 'Email đã được đăng ký.' });
   }
   if (existingUsername) {
-    return res.render('User/signup', { message: 'Username đã được sử dụng.' });
+    return res.render('User/signup',{ message: 'Username đã được sử dụng.' });
   }
   try {
     await tutor.save();
@@ -93,68 +87,69 @@ router.post('/register', async (req, res) => { //NOSONAR
   }
 });
 //Post login 
-router.post('/login', async function (req, res) { //NOSONAR 
-  const username = req.body.login;
-  const email = req.body.login;
-  const password = req.body.password;
-  try {
-    const tutor = await Tutor.findOne({ $or: [{ email: email }, { username: username }] });
-    if (!tutor) {
-      res.render('User/login', { message: 'Tên đăng nhập hoặc mật khẩu không đúng.' });
-    } else if (tutor.password !== password) {
-      res.render('User/login', { message: 'Tên đăng nhập hoặc mật khẩu không đúng.' });
-    } else if (tutor.status !== 'active') {
-      res.render('User/login', { message: 'Tài khoản bị khóa' });
-    } else {
-      req.session.name_tutor = tutor.username;
-      console.log(req.session.name_tutor);
-      req.session.loggedin_tutor = true;
-      req.session.email = email;
-      res.redirect('/tutor/home');
+  router.post('/login', async function (req, res) { //NOSONAR 
+    const email = req.body.email;
+    const password = req.body.password;
+    try {
+      const tutor = await Tutor.findOne({ email: email});
+      if (!tutor) {
+        res.render('User/login', { message: 'Tên đăng nhập hoặc mật khẩu không đúng.' });
+      } else if (tutor.password !== password) {
+        res.render('User/login', { message: 'Tên đăng nhập hoặc mật khẩu không đúng.' });
+      } else if (tutor.status !== 'active') {
+        res.render('User/login', { message: 'Tài khoản bị khóa' });
+      } else {
+        req.session.name_tutor = tutor.username;
+        console.log("Session created : ", req.session.name_tutor);
+        req.session.loggedin_tutor = true;
+        req.session.email = email;
+        res.redirect('/tutor/home');
+      }
+    } catch (err) {
+      console.error(err);
+      res.render('User/login', { message: 'Đã xảy ra lỗi khi đăng nhập.' });
     }
-  } catch (err) {
-    console.error(err);
-    res.render('User/login', { message: 'Đã xảy ra lỗi khi đăng nhập.' });
-  }
-});
-
+  });
 router.get('/profile', checkMember, async function (req, res) { //NOSONAR
-  try {
-    let tutors;
-    const usernametutor = req.session.name_tutor;
-    const emailtutor = req.session.email;
-    if (usernametutor || emailtutor) {
-      tutors = await Tutor.findOne({ $or: [{ email: emailtutor }, { username: usernametutor }] });
+    try {
+      let tutors;
+      if (req.session.email){
+        tutors = await Tutor.findOne({ email: req.session.email });
+      }
+      else {
+        tutors = await Tutor.findOne({ username: req.session.name_tutor });
+      }
+      
+      //Tìm thông tin 
+      
+      const name = tutors.username; 
+      const reviews = await Review.findOne({nametutor: name});
+      const course = await Course.findOne({nametutor: name});
+      //Định dạng chỉ ngày tháng năm cho birthday
+      const birthday = new Date(tutors.birthday);
+      const options = { day: 'numeric', month: 'numeric', year: 'numeric' };
+      const formattedBirthday = birthday.toLocaleDateString('vi-VN', options);
+      //render 
+      res.render('User/account/index.ejs', {
+        tutors,
+        reviews,
+        course,
+        formattedBirthday,
+        username: req.session.name_tutor,
+      });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
     }
-
-    //Tìm thông tin 
-    const reviews = await Review.findOne({ nametutor: usernametutor });
-    const course = await Course.findOne({ nametutor: usernametutor });
-    //Định dạng chỉ ngày tháng năm cho birthday
-    const birthday = new Date(tutors.birthday);
-    const options = { day: 'numeric', month: 'numeric', year: 'numeric' };
-    const formattedBirthday = birthday.toLocaleDateString('vi-VN', options);
-    //render 
-    res.render('User/account/index.ejs', {
-      tutors,
-      reviews,
-      course,
-      formattedBirthday,
-      username: usernametutor,
-    });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+  });
 // update profile
-router.put('/save', function (req, res) {
+router.put('/save', function(req, res) {
   const name = req.body.username;
   const email = req.body.email;
   const newEmail = req.body.newEmail;
   const phone = req.body.phonenumber;
-  const birthday = new Date(req.body.birthday);
+  const birthday= new Date(req.body.birthday);
   const address = req.body.address;
-
+ 
   // Kiểm tra xem có email mới không
   if (newEmail) {
     // Tìm kiếm người dùng với địa chỉ email mới
@@ -166,8 +161,7 @@ router.put('/save', function (req, res) {
         } else {
           // Địa chỉ email mới chưa được sử dụng, cập nhật thông tin người dùng
           Tutor.findOneAndUpdate({ email: email }, {
-            $set: { username: name, email: newEmail, phonenumber: phone, birthday: birthday, address: address }
-          }, { new: true })
+            $set: {username: name,email: newEmail, phonenumber: phone, birthday: birthday, address: address } }, { new: true })
             .then(tutor => {
               if (!tutor) {
                 res.status(404).json({ message: 'User not found' });
@@ -184,8 +178,7 @@ router.put('/save', function (req, res) {
   } else {
     // Không có email mới, cập nhật thông tin người dùng bình thường
     Tutor.findOneAndUpdate({ email: email }, {
-      $set: { username: name, phonenumber: phone, birthday: birthday, address: address }
-    }, { new: true })
+      $set: { username: name, phonenumber: phone, birthday: birthday, address: address } }, { new: true })
       .then(tutor => {
         if (!tutor) {
           res.status(404).json({ message: 'User not found' });
@@ -230,22 +223,22 @@ router.post('/avatar/update', upload.single('file'), async (req, res) => {
     const oldAvatarPath = tutor.avt;
 
     // Xóa ảnh cũ nếu tồn tại
-    // Lưu đường dẫn tương đối của thư mục public vào một biến
-    const publicDir = 'public';
+   // Lưu đường dẫn tương đối của thư mục public vào một biến
+const publicDir = 'public';
 
-    // Xóa ảnh cũ nếu tồn tại
-    if (oldAvatarPath) {
-      const fs = require('fs');
-      const filePath = path.join(publicDir, oldAvatarPath);
-      if (fs.existsSync(filePath)) {
-        fs.unlink(filePath, function (err) {
-          if (err) throw err;
-          //console.log('Old avatar deleted!');
-        });
-      } else {
-        // console.log('Avatar file not found!');
-      }
-    }
+// Xóa ảnh cũ nếu tồn tại
+if (oldAvatarPath) {
+  const fs = require('fs');
+  const filePath = path.join(publicDir, oldAvatarPath);
+  if (fs.existsSync(filePath)) {
+    fs.unlink(filePath, function (err) {
+      if (err) throw err;
+      //console.log('Old avatar deleted!');
+    });
+  } else {
+   // console.log('Avatar file not found!');
+  }
+}
 
     // Lưu ảnh mới vào DB
     await Tutor.findOneAndUpdate({ _id: req.body.tutorId }, { avt: avatarPath });
@@ -256,13 +249,13 @@ router.post('/avatar/update', upload.single('file'), async (req, res) => {
   }
 });
 //Tạo khóa học
-router.post('/new-courses', checkMember, async (req, res) => { //NOSONAR 
+router.post('/new-courses',checkMember, async (req, res) => { //NOSONAR 
   //add khóa học mới
-  const { name, category, subject, status, decs } = req.body;
+  const { name, category, subject ,status , decs } = req.body;
   const nametutor = req.session.name_tutor;
   const key = Math.floor(Math.random() * 9000) + 1000;
-  const course = await Course.findOne({ key: key });
-  if (course !== null) {
+  const course = await Course.findOne({key: key});
+  if(course !== null){
     checkKey(key);
   }
   //tạo thông báo
@@ -286,37 +279,77 @@ router.post('/new-courses', checkMember, async (req, res) => { //NOSONAR
     // Lưu course mới vào CSDL
     await newNotification.save();
     await newCourse.save();
-    console.log("Tên khóa mới ", name, "Mã: ", key);
+    console.log("Tên khóa mới ",name,"Mã: ",key);
     // Gửi phản hồi về client
   } catch (error) {
-    res.render('User/main/course', { message: 'Tạo khóa học thất bại!', })
+    res.render('User/main/course',{ message: 'Tạo khóa học thất bại!',})
     console.log(error);
   }
 });
-
-
-//Functions
-// Function check member
-function checkMember(req, res, next) {
+//search 
+router.post('/search',checkMember, async (req, res) => { //NOSONAR
+  let {grade , subject } = req.body;
+  let query = req.body.query.trim();
+  let courses; 
+  const status = 'active';
+  console.log({status , grade, subject, query });
   try {
-    if (req.session.loggedin_tutor) {
-      next();
+    if (query) {
+      courses = await Course.find({
+        status,
+        category: new RegExp('.*' + grade + '.*', 'i'),
+        subject: new RegExp('.*' + subject + '.*', 'i'),
+        $or: [
+          {name: new RegExp('.*' + query + '.*', 'i')},
+          {decs: new RegExp('.*' + query + '.*', 'i')},
+          {key: new RegExp('.*' + query + '.*', 'i')},
+          {nametutor: new RegExp('.*' + query + '.*', 'i')}
+        ]
+      });
+    }  else if (grade){
+      courses = await Course.find({
+        status,
+        category: new RegExp('.*' + grade + '.*', 'i'),
+        $or: [
+        {subject: new RegExp('.*' + subject + '.*', 'i')}
+      ]
+      });
     } else {
-      res.redirect('/tutor/login');
+      courses = await Course.find({
+          status
+        }
+      );
+    }
+      res.render('User/main/index',
+          {
+              courses,
+          });
+  } catch (err) {
+      res.status(500).json({ message: error.message })
+  }
+});
+//Functions
+  // Function check member
+  function checkMember(req, res, next){
+    try {
+    if (req.session.loggedin_tutor) {
+        next();  
+    } else {  
+        res.redirect('/tutor/login');
     }
   }
   catch (error) {
     res.status(500).json({ message: 'Lỗi' })
+    }
   }
-}
-//RANDOM KEY
-function checkKey(key) {
-  const newKey = Math.floor(Math.random() * 9000) + 1000;
-  if (newKey === key) {
-    return checkKey(key);
-  } else {
-    return newKey;
+  //RANDOM KEY
+  function checkKey(key) {
+    const newKey = Math.floor(Math.random() * 9000) + 1000;
+    if (newKey === key) {
+      return checkKey(key);
+    } else {
+      return newKey;
+    }
   }
-}
-
+  
 module.exports = router; 
